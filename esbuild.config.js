ae2e492c4b3fa71ba25d58df41bf2f66b653f3f5
samuelmatarese <samuel.matarese@protonmail.com>
2025-06-22 13:05:47 +0200

    improve esbuild

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,15 +1,30 @@
 const esbuild = require("esbuild");
 const path = require("path");
 
-esbuild.build({
-  entryPoints: ["src/webview/index.ts"],
-  bundle: true,
-  outfile: "out/webview/webview.js",
-  platform: "browser",
-  sourcemap: true,
-  minify: false,
-  target: ["es2022"],
-  define: {
-    "process.env.NODE_ENV": '"production"',
-  },
-}).catch(() => process.exit(1));
+esbuild
+  .build({
+    entryPoints: ["src/extension.ts"],
+    bundle: true,
+    outfile: "out/extension.js",
+    platform: "node",
+    target: ["node20"],
+    sourcemap: true,
+    minify: false,
+    external: ["vscode"],
+  })
+  .catch(() => process.exit(1));
+
+esbuild
+  .build({
+    entryPoints: ["src/webview/index.ts"],
+    bundle: true,
+    outfile: "out/webview/webview.js",
+    platform: "browser",
+    target: ["es2022"],
+    sourcemap: true,
+    minify: false,
+    define: {
+      "process.env.NODE_ENV": '"production"',
+    },
+  })
+  .catch(() => process.exit(1));
